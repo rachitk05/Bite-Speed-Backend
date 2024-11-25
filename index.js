eee78ffbcd764304a5666081a56403e1c969d39e
rachitk05 <rachit.k23csai@nst.rishihood.edu.in>
2024-11-25 15:32:24 +0530

    adding cors to integrate with frontend

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-import express from "express"
-import {connectDB} from "./db.js";
-const app = express();
-const port = process.env.PORT || 3000;
+import express from "express";
+import cors from "cors"; // Add this line
+import { connectDB } from "./db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 
-app.use(express.json())
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors()); // Add this line
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API is running!");
 });
 
-
-app.use('/',contactRoutes)
-
-
+app.use('/', contactRoutes);
 
 connectDB().then(() => {
     app.listen(port, () => {
@@ -22,4 +24,3 @@ connectDB().then(() => {
 }).catch((error) => {
     console.error('Failed to connect to the database:', error.message);
 });
-
